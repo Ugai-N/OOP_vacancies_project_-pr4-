69290f26249995dec7ed41634c2674df5c7173bb
Nadezhda Ugay <777ugay@gmail.com>
2023-07-18 16:35:46 +0300

    added functions and updated API.py for HH

--- src/API.py
+++ src/API.py
@@ -1,7 +1,8 @@
-import json
 from abc import ABC, abstractmethod
 
 import requests
+
+from src.vacancy import Vacancy
 
 
 class AbstractAPI(ABC):
@@ -9,18 +10,58 @@ class AbstractAPI(ABC):
     Наследники класса для каждой платформы подключаются к API и получают вакансии'''
 
     @abstractmethod
-    def get_vacancies(self):
+    def check_connection(self) -> int:
+        '''функция для проверки статус-кода при работе с API'''
+        pass
+
+    @abstractmethod
+    def get_vacancies(self, search_query: str) -> list:
+        '''обращается к API и выгружает список вакансии согласно запросу search_query'''
+        pass
+
+    @abstractmethod
+    def initialize_vacancy(self, search_query: str) -> None:
+        '''для каждой вакансии, найденной по запросу search_query, инициализирует экземпляр Vacancy'''
         pass
 
 
 class HeadHunterAPI(AbstractAPI):
-    def get_vacancies(self):
-        data = requests.get('https://api.hh.ru/vacancies').json()
+    def check_connection(self) -> int:
+        response = requests.get('https://api.hh.ru/vacancies')
+        return response.status_code
+
+    def get_vacancies(self, search_query: str) -> list:
+        data = []
+        for page in range(0, 20):
+            params = {
+                      'text': search_query,
+                      'page': page,
+                      'per_page': 10
+                     }
+            raw_data = requests.get('https://api.hh.ru/vacancies', params=params).json()
+            data.extend(raw_data['items'])
         return data
-        # return len(data['items'])
-        # return data['found']
+
+    @staticmethod
+    def qty_vacancies(self, search_query: str) -> str:
+        params = {
+                  'text': search_query,
+                  'per_page': 100
+                 }
+        raw_data = requests.get('https://api.hh.ru/vacancies', params=params).json()
+        return f"По запросу '{search_query}' найдено {raw_data['found']} вакансий, возможно отразить только 2000"
+
+    def initialize_vacancy(self, search_query: str) -> None:
+        for i in self.get_vacancies(search_query):
+            Vacancy(i['name'], i['area']['name'], i['alternate_url'], i['salary'], i['snippet'])
 
 
-class SuperJobApi(AbstractAPI):
-    def get_vacancies(self):
+class SuperJobAPI(AbstractAPI):
+    def check_connection(self) -> int:
+        pass
+
+    def get_vacancies(self, search_query: str) -> list:
+        pass
+
+    def initialize_vacancy(self, search_query: str) -> None:
         pass
